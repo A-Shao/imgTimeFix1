001dd075e5de1215313cdfb41b77b0cf346d189e
AlexS09 <Alexshao.24@outlook.com>
2018-08-15 22:22:33 +0800

    Initial commit

---
+++ imgTimeFix1/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace imgTimeFix1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.scanBtn1 = new System.Windows.Forms.Button();
+            this.editBtn1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pathInput1 = new System.Windows.Forms.TextBox();
+            this.typeInput1 = new System.Windows.Forms.TextBox();
+            this.newName = new System.Windows.Forms.TextBox();
+            this.newCreation = new System.Windows.Forms.TextBox();
+            this.newType = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.creationTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imgTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.switchBtn1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.54656F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.45344F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 203F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1034, 569);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.scanBtn1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.editBtn1, 0, 4);
+            this.tableLayoutPanel3.Controls.Add(this.switchBtn1, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(833, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 8;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.43781F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.43781F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.43781F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.43781F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.43781F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.43781F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.68657F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.68657F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(198, 543);
+            this.tableLayoutPanel3.TabIndex = 6;
+            // 
+            // scanBtn1
+            // 
+            this.scanBtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scanBtn1.Location = new System.Drawing.Point(3, 3);
+            this.scanBtn1.Name = "scanBtn1";
+            this.scanBtn1.Size = new System.Drawing.Size(192, 53);
+            this.scanBtn1.TabIndex = 2;
+            this.scanBtn1.Text = "SCAN";
+            this.scanBtn1.UseVisualStyleBackColor = true;
+            this.scanBtn1.Click += new System.EventHandler(this.scanBtn1_Click);
+            // 
+            // editBtn1
+            // 
+            this.editBtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editBtn1.Location = new System.Drawing.Point(3, 271);
+            this.editBtn1.Name = "editBtn1";
+            this.editBtn1.Size = new System.Drawing.Size(192, 53);
+            this.editBtn1.TabIndex = 2;
+            this.editBtn1.Text = "EDIT";
+            this.editBtn1.UseVisualStyleBackColor = true;
+            this.editBtn1.Click += new System.EventHandler(this.editBtn1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.pathInput1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.typeInput1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.newName, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.newCreation, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.newType, 0, 5);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(589, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 9;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(238, 563);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // pathInput1
+            // 
+            this.pathInput1.Location = new System.Drawing.Point(3, 3);
+            this.pathInput1.Name = "pathInput1";
+            this.pathInput1.Size = new System.Drawing.Size(232, 25);
+            this.pathInput1.TabIndex = 1;
+            this.pathInput1.Text = "Folder Path 1";
+            // 
+            // typeInput1
+            // 
+            this.typeInput1.Location = new System.Drawing.Point(3, 65);
+            this.typeInput1.Name = "typeInput1";
+            this.typeInput1.Size = new System.Drawing.Size(232, 25);
+            this.typeInput1.TabIndex = 2;
+            this.typeInput1.Text = "File Type";
+            // 
+            // newName
+            // 
+            this.newName.Location = new System.Drawing.Point(3, 189);
+            this.newName.Name = "newName";
+            this.newName.Size = new System.Drawing.Size(232, 25);
+            this.newName.TabIndex = 3;
+            this.newName.Text = "Change Name";
+            // 
+            // newCreation
+            // 
+            this.newCreation.Location = new System.Drawing.Point(3, 251);
+            this.newCreation.Name = "newCreation";
+            this.newCreation.Size = new System.Drawing.Size(232, 25);
+            this.newCreation.TabIndex = 4;
+            this.newCreation.Text = "Change Creation Time";
+            // 
+            // newType
+            // 
+            this.newType.Location = new System.Drawing.Point(3, 313);
+            this.newType.Name = "newType";
+            this.newType.Size = new System.Drawing.Size(232, 25);
+            this.newType.TabIndex = 5;
+            this.newType.Text = "Change File Type";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.creationTime,
+            this.type,
+            this.imgTime});
+            this.listView1.FullRowSelect = true;
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(580, 563);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // name
+            // 
+            this.name.Tag = "";
+            this.name.Text = "Name";
+            this.name.Width = 114;
+            // 
+            // creationTime
+            // 
+            this.creationTime.Text = "Creation Time";
+            this.creationTime.Width = 150;
+            // 
+            // type
+            // 
+            this.type.Text = "File Type";
+            this.type.Width = 113;
+            // 
+            // imgTime
+            // 
+            this.imgTime.Text = "Image Taken Time";
+            this.imgTime.Width = 149;
+            // 
+            // switchBtn1
+            // 
+            this.switchBtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.switchBtn1.Location = new System.Drawing.Point(3, 70);
+            this.switchBtn1.Name = "switchBtn1";
+            this.switchBtn1.Size = new System.Drawing.Size(192, 51);
+            this.switchBtn1.TabIndex = 3;
+            this.switchBtn1.Text = "Switch";
+            this.switchBtn1.UseVisualStyleBackColor = true;
+            this.switchBtn1.Click += new System.EventHandler(this.switchBtn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 593);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox pathInput1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button scanBtn1;
+        private System.Windows.Forms.Button editBtn1;
+        private System.Windows.Forms.TextBox typeInput1;
+        private System.Windows.Forms.TextBox newName;
+        private System.Windows.Forms.TextBox newCreation;
+        private System.Windows.Forms.TextBox newType;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader creationTime;
+        private System.Windows.Forms.ColumnHeader type;
+        private System.Windows.Forms.ColumnHeader imgTime;
+        private System.Windows.Forms.Button switchBtn1;
+    }
+}
+
---
+++ imgTimeFix1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imgTimeFix1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+
+        //file variables
+        private DirectoryInfo folder1;
+        private List<FileInfo> files;
+        private List<FileInfo> files1;
+        private String[] fileTypes;
+
+
+        //set variables
+        public void setVariables(String p)
+        {
+
+            folder1 = new DirectoryInfo(p);
+            files = new List<FileInfo>();
+
+            scan(folder1);
+
+            fileTypes = new String[files.Count];
+            for (int i = 0; i < files.Count; i++)
+            {
+                fileTypes[i] = files[i].Extension;
+
+            }
+
+        }
+
+        private void scan(DirectoryInfo dir)
+        {
+            try
+            {
+
+                FileSystemInfo[] fileInfo = dir.GetFileSystemInfos();
+
+                foreach (FileSystemInfo f in fileInfo)
+                {
+                    if (f != null)
+                    {
+                        if (f is DirectoryInfo)
+                        {
+                            DirectoryInfo subdir = new DirectoryInfo(f.FullName);
+                            scan(subdir);
+                        }
+
+                        else
+                        {
+                            files.Add((FileInfo)f);
+                        }
+                    }
+
+                }
+            }
+
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        #endregion
+
+        #region All File Info
+
+        //list file info
+        public void listFileInfo(int n)
+        {
+            //clear list
+            //listView1.Clear();
+
+            //get type
+            String tp = typeInput1.Text;
+
+            //file amount3
+            int l = files.Count;
+
+            //file info
+            String[] fileNames = new String[l];
+            String[] creationTime = new String[l];
+            String[] takenDate = new String[l];
+
+            //get info
+            for (int i = 0; i < files.Count; i++)
+            {
+                fileNames[i] = files[i].Name;
+                creationTime[i] = files[i].CreationTime.ToString();
+
+                if (files[i].Extension == ".jpg")
+                    takenDate[i] = getImageTakenDate(files[i].FullName).ToString();
+
+            }
+
+            //fill info list
+            for (int i = 0; i < files.Count; i++)
+            {
+                String s = files[i].Extension;
+
+                if (tp.Equals("File Type") || s.ToLower().Equals(tp.ToLower()))
+                {
+                    ListViewItem item = listView1.Items.Add(fileNames[i]);
+
+                    item.SubItems.Add(creationTime[i]);
+                    item.SubItems.Add(fileTypes[i]);
+                    item.SubItems.Add(takenDate[i]);
+                }
+
+            }
+
+        }
+
+        //get take picture date
+        private String getImageTakenDate(String f)
+        {
+            Encoding ascii = Encoding.ASCII;
+            string picDate;
+
+            FileStream stream = new FileStream(f, FileMode.Open, FileAccess.Read);
+            Image image = Image.FromStream(stream, true, false);
+
+            foreach (PropertyItem p in image.PropertyItems)
+            {
+                //获取拍摄日期时间
+
+                if (p.Id == 0x9003) // 0x0132 最后更新时间
+                {
+                    stream.Close();
+                    picDate = ascii.GetString(p.Value);
+                    if ((!"".Equals(picDate)) && picDate.Length >= 10)
+                    {
+                        // 拍摄日期
+                        String pd = picDate.Substring(0, 10);
+                        pd = pd.Replace(":", "/");
+                        pd += picDate.Substring(10);
+                        return pd;
+                    }
+                }
+
+            }
+            stream.Close();
+            return "";
+        }
+
+        #endregion
+
+        #region Modify File Info
+
+        //change properties
+        private void changePprt()
+        {
+
+            int i = listView1.SelectedItems[0].Index - 1;
+
+            if (newName.Text != "Change Name")
+            {
+                String st = files[i].FullName;
+                st = st.Substring(0, st.IndexOf(files[i].Name));
+                st += newName.Text + files[i].Extension;
+
+                files[i].MoveTo(st);
+
+                MessageBox.Show("Name changed to \n" + files[i].Name);
+            }
+
+            if (newCreation.Text != "Change Creation Time")
+            {
+                files[i].CreationTime = DateTime.Parse(newCreation.Text);
+
+                MessageBox.Show("Creation Time changed to \n" + files[i].CreationTime);
+            }
+            
+            if (newType.Text != "Change File Type")
+            {
+                String st = files[i].FullName;
+                st = st.Substring(0, st.IndexOf(files[i].Extension));
+                st += newType.Text;
+
+                files[i].MoveTo(st);
+
+                MessageBox.Show("Type changed to \n" + files[i].Extension);
+            }
+
+        }
+
+        #endregion
+
+        #region Events
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanBtn1_Click(object sender, EventArgs e)
+        {
+            String path1 = pathInput1.Text;
+
+            setVariables(path1);
+            listFileInfo(1);
+
+            files1 = files;
+            
+        }
+
+        private void editBtn1_Click(object sender, EventArgs e)
+        {
+            changePprt();
+            MessageBox.Show("Edit Completed");
+        }
+
+        private void switchBtn1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < files.Count; i++)
+            {
+                String t = getImageTakenDate(files[i].FullName);
+                files[i].CreationTime = DateTime.Parse(t);
+            }
+
+            MessageBox.Show("Time Switched");
+        }
+
+        #endregion
+    }
+}
